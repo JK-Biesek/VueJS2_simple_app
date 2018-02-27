803402dfed108c2f73b7a70b1ff2eefe320ebb7c
jakub <jakub.webdesign@gmail.com>
2018-02-27 21:37:35 +0100

    vueJS2

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <skills msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import skills from './components/skills.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    skills
   }
 }
 </script>
