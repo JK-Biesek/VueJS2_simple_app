fadde4143f45e3a7508f39f6d2d36472b27ad3a7
jakub <jakub.webdesign@gmail.com>
2018-02-28 08:55:54 +0100

    add veevalidator form

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="hello">
   <div class="holder">
+  <form @submit.prevent="addSkill">
+    <input type="text" placeholder="Enter your skill" v-model="skill"   v-validate="'min:5'" name="skill">
+    <p class="alert" v-if="errors.has('skill')">{{errors.first('skill')}}</p>
+    {{skill}}
+    </form>
     <ul>
-      <li v-for="(data,index) in skills" :key='index'>{{index}}. {{data.skill}} </li>
+      <li v-for="(data,index) in skills" :key='index'> {{data.skill}} </li>
     </ul>
-    <p v-if="skills.length >= 1">You have more than one skill</p>
-    <p v-else>You have less than 1 skill</p>
+  <p>Your skills </p>
     </div>
   </div>
 </template>
@@ -15,30 +19,28 @@ export default {
   name: 'skills',
   data(){
   return {
+  skill: '',
   skills: [
     {"skill" : "Php"},
     {"skill" : "OOP"},
     {"skill" : "VueJs"},
       ]
     }
+  },
+  methods: {
+    addSkill() {
+    this.$validator.validateAll().then((result) =>{
+    if(result){
+    this.skills.push({skill: this.skill})
+    this.skill = '';
+    }
+    })
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style src="./style.css" scoped>
+
 </style>
